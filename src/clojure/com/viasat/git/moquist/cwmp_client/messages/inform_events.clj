d91a27e55a9951295b65e8546c0351aa5dd9056c
Matt Oquist <matthew.oquist@viasat.com>
2025-02-05 19:01:17 +0000

    add composition fns for informs

---
+++ src/clojure/com/viasat/git/moquist/cwmp_client/messages/inform_events.clj
@@ -0,0 +1,46 @@
+(ns com.viasat.git.moquist.cwmp-client.messages.inform-events
+  (:require [com.viasat.git.moquist.cwmp-client.messages :as messages]
+            [com.viasat.git.moquist.cwmp-client.stateful-device :as stateful-device]))
+
+(defn compose-bootstrap-boot [stateful-device]
+  (let [{:as DeviceId :keys [SerialNumber]} (stateful-device/get-device-id stateful-device)
+        ParameterList (stateful-device/get-parameter-values stateful-device [])
+        Event ["0 BOOTSTRAP" "1 BOOT"]]
+    (->> (messages/tr069-inform
+          SerialNumber
+          {:DeviceId DeviceId
+           :ParameterList ParameterList
+           :Event Event}))))
+
+(defn compose-periodic-inform [stateful-device]
+  (let [{:as DeviceId :keys [SerialNumber]} (stateful-device/get-device-id stateful-device)
+        ProvisioningCode (stateful-device/get-parameter-values stateful-device ["Device.DeviceInfo.ProvisioningCode"])
+        ParameterList (stateful-device/get-parameter-values stateful-device [])
+        Event ["2 PERIODIC"]]
+    (->> (messages/tr069-inform
+          SerialNumber
+          {:DeviceId DeviceId
+           :ParameterList (merge ParameterList ProvisioningCode)
+           :Event Event}))))
+
+(defn compose-boot [stateful-device]
+  (let [{:as DeviceId :keys [SerialNumber]} (stateful-device/get-device-id stateful-device)
+        ParameterList (stateful-device/get-parameter-values stateful-device [])
+        Event ["1 BOOT"]]
+    (->> (messages/tr069-inform
+          SerialNumber
+          {:DeviceId DeviceId
+           :ParameterList ParameterList
+           :Event Event}))))
+
+(defn compose-value-change [stateful-device parameter-names]
+  (let [{:as DeviceId :keys [SerialNumber]} (stateful-device/get-device-id stateful-device)
+        ParameterList (stateful-device/get-parameter-values stateful-device parameter-names)
+        Event ["4 VALUE CHANGE"]]
+    (->> (messages/tr069-inform
+          SerialNumber
+          {:DeviceId DeviceId
+           :ParameterList ParameterList
+           :Event Event}))))
+
+
