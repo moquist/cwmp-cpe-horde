882c23b141bef3676fa13313640bcdeedffe0a55
Matt Oquist <matthew.oquist@viasat.com>
2025-02-21 00:44:51 +0000

    add nonce fns required for Connection Request support

---
+++ test/clojure/com/viasat/git/moquist/ring/middleware/http_digest/nonce_test.clj
@@ -0,0 +1,15 @@
+(ns com.viasat.git.moquist.ring.middleware.http-digest.nonce-test
+  (:require [clojure.test :refer [deftest is]]
+            [com.viasat.git.moquist.ring.middleware.http-digest.nonce :as nonce]))
+
+(set! *warn-on-reflection* true)
+
+(deftest nonce-valid?-test
+  (let [nonce (nonce/generate-nonce)]
+    (nonce/add-nonce! nonce)
+    (is (nonce/nonce-valid? nonce)))
+  (binding [nonce/*nonce-timeout-millis* 0]
+    (let [nonce (nonce/generate-nonce)]
+      (nonce/add-nonce! nonce)
+      (Thread/sleep 1)
+      (is (not (nonce/nonce-valid? nonce))))))
