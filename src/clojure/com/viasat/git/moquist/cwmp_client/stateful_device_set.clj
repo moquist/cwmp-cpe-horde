e94cfd0bc1b23be60a104f8617247f96b8a2cb82
Matt Oquist <matthew.oquist@viasat.com>
2025-02-21 15:59:32 +0000

    add regular fns for StatefulDeviceSet protocol methods

--- src/clojure/com/viasat/git/moquist/cwmp_client/stateful_device_set.clj
+++ src/clojure/com/viasat/git/moquist/cwmp_client/stateful_device_set.clj
@@ -3,3 +3,11 @@
 (defprotocol StatefulDeviceSet
   (-get-device [this device-id])
   (-list-devices [this]))
+
+(defn get-device [stateful-device-set device-id]
+  (assert (satisfies? StatefulDeviceSet stateful-device-set))
+  (-get-device stateful-device-set device-id))
+
+(defn list-devices [stateful-device-set]
+  (assert (satisfies? StatefulDeviceSet stateful-device-set))
+  (-list-devices stateful-device-set))
